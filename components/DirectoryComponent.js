61a22eb92273ba8befa4e027eaf3688c1a2ded94
Alicia Lancaster <alancaster123@yahoo.com>
2020-04-24 16:00:20 -0700

    Navigation Part 1

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,29 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
 
-function Directory(props) {
-  //receives props from Main component (parent)
+class Directory extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      campsites: CAMPSITES,
+    };
+  }
 
-  const renderDirectoryItem = ({ item }) => {
-    //use destructuring; property called item, will iterate over items in array to put in ListItem
-    return (
-      <ListItem
-        title={item.name} //campsite names
-        subtitle={item.description} //campsite description
-        onPress={() => props.onPress(item.id)} //when this component is pressed the function will fire; trigger the onPress campsite selector passing props
-        leftAvatar={{ source: require('./images/react-lake.jpg') }} //campsite image
-      />
-    );
+  //Header title for navigation
+  static navigationOptions = {
+    title: 'Directory',
   };
 
-  return (
-    <FlatList
-      data={props.campsites} //pass campsites array
-      renderItem={renderDirectoryItem} //call back function
-      keyExtractor={(item) => item.id.toString()} //use id number as unique key from campsite array
-    />
-  );
+  render() {
+    const { navigate } = this.props.navigation;
+    const renderDirectoryItem = ({ item }) => {
+      //use destructuring; property called item, will iterate over items in array to put in ListItem
+      return (
+        <ListItem
+          title={item.name} //campsite names
+          subtitle={item.description} //campsite description
+          onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })} //when this component is pressed the function will fire; trigger the onPress campsite selector passing props
+          leftAvatar={{ source: require('./images/react-lake.jpg') }} //campsite image
+        />
+      );
+    };
+
+    return (
+      <FlatList
+        data={this.state.campsites} //pass from current state campsites array
+        renderItem={renderDirectoryItem} //call back function
+        keyExtractor={(item) => item.id.toString()} //use id number as unique key from campsite array
+      />
+    );
+  }
 }
 
 export default Directory;
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,41 +1,44 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
-import { View } from 'react-native';
-import { CAMPSITES } from '../shared/campsites';
+import { View, Platform } from 'react-native';
+import { createStackNavigator } from 'react-navigation'; //main will hold all navigators
+
+const DirectoryNavigator = createStackNavigator(
+  {
+    //one argument with the components available for this stack
+    Directory: { screen: Directory },
+    CampsiteInfo: { screen: CampsiteInfo },
+  },
+  {
+    //optinonal second argument
+    //defaults to show this component
+    initialRouteName: 'Directory',
+    //Nav options are set with various objects that configure the settings for the navigator
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: '#5637DD',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        color: '#fff',
+      },
+    },
+  }
+);
 
 class Main extends Component {
-  //container component to presentational components
-  constructor(props) {
-    super(props);
-    this.state = {
-      campsites: CAMPSITES, //holds campsite data
-      selectedCampsite: null, //reserves space in local state that keeps track of which campsite is selected
-    };
-  }
-
-  onCampsiteSelect(campsiteId) {
-    //event handlers gets passed campsiteID that will update this selectedCampsite property when selected
-    this.setState({ selectedCampsite: campsiteId }); //updates the state
-  }
-
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        <Directory
-          campsites={this.state.campsites} //pass the state data to Directory component
-          onPress={(campsiteId) => this.onCampsiteSelect(campsiteId)}
-          //2nd prop called onPress; pass an arrow function that takes campsiteId as the parameter and contains the onCampsiteSelect event handler inside the function body
-        />
-        <CampsiteInfo
-          campsite={
-            //pass prop called campsite
-            this.state.campsites.filter(
-              //need to take the whole array and filters the campsite with the matching id
-              (campsite) => campsite.id === this.state.selectedCampsite
-            )[0] //returns an array so we will grab the 1st item in the array at index 0
-          }
-        />
+      <View
+        style={{
+          flex: 1,
+          //changes the padding based on platform
+          paddingTop:
+            Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight,
+        }}
+      >
+        <DirectoryNavigator />
       </View>
     );
   }
