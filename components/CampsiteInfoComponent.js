d88f14dd30e35ab343c69a3d10ed914eeede486b
Alicia Lancaster <alancaster123@yahoo.com>
2020-05-05 20:12:42 -0700

    Gestures Part 2

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -39,10 +39,19 @@ function RenderCampsite(props) {
 
   const { campsite } = props; //still need to have the destructured campsite object in this function
 
+  const view = React.createRef(); //similar to getElememtbyId using Ref
+
   const recognizeDrag = ({ dx }) => (dx < -200 ? true : false); //dx= distance across x access
 
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
+    onPanResponderGrant: () => {
+      view.current
+        .rubberBand(1000)
+        .then((endState) =>
+          console.log(endState.finished ? 'finished' : 'canceled')
+        );
+    },
     onPanResponderEnd: (e, gestureState) => {
       console.log('pan responder end', gestureState);
       if (recognizeDrag(gestureState)) {
@@ -76,6 +85,7 @@ function RenderCampsite(props) {
         animation="fadeInDown"
         duration={2000}
         delay={1000}
+        ref={view}
         {...panResponder.panHandlers}
       >
         <Card
